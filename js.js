12ba91540ed6c9b7dea596d4052cad43d7a2f78e
sahasra674 <23409sg@hvhs.school.nz>
2025-07-04 04:31:51 +0000

    Jul 4, 2025, 4:31 AM

---
+++ js.js
@@ -0,0 +1,20 @@
+ <div class="item">
+  <img src="https://via.placeholder.com/180x150" alt="Gold Ring">
+  <h3>Gold Ring</h3>
+  <p class="price">$120</p>
+  <button onclick="addToCart(0)">Add to Cart</button>
+</div>
+
+<div class="item">
+  <img src="https://via.placeholder.com/180x150" alt="Silver Necklace">
+  <h3>Silver Necklace</h3>
+  <p class="price">$85</p>
+  <button onclick="addToCart(1)">Add to Cart</button>
+</div>
+
+<div class="item">
+  <img src="https://via.placeholder.com/180x150" alt="Diamond Earrings">
+  <h3>Diamond Earrings</h3>
+  <p class="price">$150</p>
+  <button onclick="addToCart(2)">Add to Cart</button>
+</div>
